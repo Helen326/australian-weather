d8702d24eac7b7635769b9941e4bef9e3e77b766
Helen326 <36986888+Helen326@users.noreply.github.com>
2021-10-21 11:51:10 +0100

    Update create-plot.R

--- R/create-plot.R
+++ R/create-plot.R
@@ -16,7 +16,7 @@ df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
   labs(title = "Brisbane") +
-  geom_point(color = 'red')
+  geom_point(color = 'green')
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
